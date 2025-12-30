2b48d6c68809d21976fdcf4960eb09fdf2427d66
MinJaeSon <mjsonsj@gmail.com>
2025-12-30 23:12:55 +0900

    feat: 게시글 도우미 지원 목록 컨트롤러 추가

--- match-service/src/main/java/com/lgcns/bebee/match/presentation/HelperApplicationController.java
+++ match-service/src/main/java/com/lgcns/bebee/match/presentation/HelperApplicationController.java
@@ -2,7 +2,9 @@ package com.lgcns.bebee.match.presentation;
 
 import com.lgcns.bebee.match.application.usecase.ApplyHelperUseCase;
 import com.lgcns.bebee.match.application.usecase.GetHelperApplicationPostsUseCase;
+import com.lgcns.bebee.match.application.usecase.GetHelperApplicationsByPostUseCase;
 import com.lgcns.bebee.match.presentation.dto.req.HelperApplyReqDTO;
+import com.lgcns.bebee.match.presentation.dto.res.HelperApplicantsByPostGetResDTO;
 import com.lgcns.bebee.match.presentation.dto.res.HelperApplicationPostsGetResDTO;
 import com.lgcns.bebee.match.presentation.swagger.HelperApplicationSwagger;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ public class HelperApplicationController implements HelperApplicationSwagger {
 
     private final ApplyHelperUseCase applyHelperUseCase;
     private final GetHelperApplicationPostsUseCase getHelperApplicationPostsUseCase;
+    private final GetHelperApplicationsByPostUseCase getHelperApplicationsByPostUseCase;
 
     @PostMapping
     public ResponseEntity<Void> apply(@RequestBody HelperApplyReqDTO request) {
@@ -36,5 +39,19 @@ public class HelperApplicationController implements HelperApplicationSwagger {
         return ResponseEntity.ok(response);
     }
 
-//    @GetMapping("/applicants")
+    @GetMapping("/posts/{postId}/applicants")
+    public ResponseEntity<HelperApplicantsByPostGetResDTO> getHelperApplicationsByPost(
+            @PathVariable String postId,
+            @RequestParam String memberId
+    ) {
+        GetHelperApplicationsByPostUseCase.Param param = new GetHelperApplicationsByPostUseCase.Param(
+                Long.parseLong(memberId),
+                Long.parseLong(postId)
+        );
+        GetHelperApplicationsByPostUseCase.Result result = getHelperApplicationsByPostUseCase.execute(param);
+
+        HelperApplicantsByPostGetResDTO response = HelperApplicantsByPostGetResDTO.from(result);
+
+        return ResponseEntity.ok(response);
+    }
 }
