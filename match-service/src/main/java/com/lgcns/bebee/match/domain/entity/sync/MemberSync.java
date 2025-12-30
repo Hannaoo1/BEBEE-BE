64b8a162a742cce5a7fc6f1d36dc019f6a645476
MinJaeSon <mjsonsj@gmail.com>
2025-12-30 17:30:34 +0900

    fix: 참조 회원 테이블명 수정 반영

--- match-service/src/main/java/com/lgcns/bebee/match/domain/entity/sync/MemberSync.java
+++ match-service/src/main/java/com/lgcns/bebee/match/domain/entity/sync/MemberSync.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "match_member_sync")
+@Table(name = "member_sync")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberSync {
