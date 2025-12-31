c711a40f0c6980595ad0a4542de1069335636755
MinJaeSon <mjsonsj@gmail.com>
2025-12-31 16:25:32 +0900

    chore: 기간 지난 도움 요청 게시글은 제외하여 검색

--- match-service/src/main/java/com/lgcns/bebee/match/domain/repository/PostRepository.java
+++ match-service/src/main/java/com/lgcns/bebee/match/domain/repository/PostRepository.java
@@ -3,8 +3,9 @@ package com.lgcns.bebee.match.domain.repository;
 import com.lgcns.bebee.match.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
-    List<Post> findAllByMemberId(Long memberId);
+    List<Post> findAllByMemberIdAndPeriodStartDateGreaterThanEqual(Long memberId, LocalDate currentDate);
 }
