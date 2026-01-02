ff838d5ac1c6b1d04f48a11a34995a35018f5bd4
MinJaeSon <mjsonsj@gmail.com>
2026-01-02 23:25:43 +0900

    chore: 토스페이먼츠 속성 객체 생성

---
+++ payment-service/src/main/java/com/lgcns/bebee/payment/infrastructure/toss/TossPaymentsProperties.java
@@ -0,0 +1,16 @@
+package com.lgcns.bebee.payment.infrastructure.toss;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "toss.payments")
+@Getter
+public class TossPaymentsProperties {
+    private String baseUrl = "https://api.tosspayments.com";
+    private String secretKey;
+    private String clientKey;
+    private int timeout = 10000;
+    private String apiVersion = "2022-11-16";
+}
