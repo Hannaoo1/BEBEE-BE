fd679ba2485535ebb603c8a41701c28f8442f021
MinJaeSon <mjsonsj@gmail.com>
2026-01-03 01:37:59 +0900

    chore: 토스 API 연결 테스트 성공

--- payment-service/src/main/java/com/lgcns/bebee/payment/infrastructure/toss/TossPaymentsProperties.java
+++ payment-service/src/main/java/com/lgcns/bebee/payment/infrastructure/toss/TossPaymentsProperties.java
@@ -1,12 +1,14 @@
 package com.lgcns.bebee.payment.infrastructure.toss;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "toss.payments")
 @Getter
+@Setter
 public class TossPaymentsProperties {
     private String baseUrl = "https://api.tosspayments.com";
     private String secretKey;
