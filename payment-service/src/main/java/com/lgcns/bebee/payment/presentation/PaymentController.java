dd0e4bc92bb18128e93c160757dd013f147e2601
MinJaeSon <mjsonsj@gmail.com>
2026-01-05 10:32:01 +0900

    feat: 결제 승인 dto 및 컨트롤러 생성

--- payment-service/src/main/java/com/lgcns/bebee/payment/presentation/PaymentController.java
+++ payment-service/src/main/java/com/lgcns/bebee/payment/presentation/PaymentController.java
@@ -1,7 +1,11 @@
 package com.lgcns.bebee.payment.presentation;
 
+import com.lgcns.bebee.common.annotation.CurrentMember;
+import com.lgcns.bebee.payment.application.usecase.ConfirmPaymentUseCase;
 import com.lgcns.bebee.payment.application.usecase.PreparePaymentUseCase;
+import com.lgcns.bebee.payment.presentation.dto.req.ConfirmPaymentReqDTO;
 import com.lgcns.bebee.payment.presentation.dto.req.PreparePaymentReqDTO;
+import com.lgcns.bebee.payment.presentation.dto.res.ConfirmPaymentResDTO;
 import com.lgcns.bebee.payment.presentation.dto.res.PreparePaymentResDTO;
 import com.lgcns.bebee.payment.presentation.swagger.PaymentSwagger;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +21,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController implements PaymentSwagger {
 
     private final PreparePaymentUseCase preparePaymentUseCase;
+    private final ConfirmPaymentUseCase confirmPaymentUseCase;
 
     @Override
     @PostMapping("/prepare")
     public ResponseEntity<PreparePaymentResDTO> preparePayment(
+            @CurrentMember Long currentMemberId,
             @RequestBody PreparePaymentReqDTO reqDTO
     ) {
-        PreparePaymentUseCase.Param param = reqDTO.toParam();
+        PreparePaymentUseCase.Param param = reqDTO.toParam(currentMemberId);
         PreparePaymentUseCase.Result result = preparePaymentUseCase.execute(param);
         PreparePaymentResDTO resDTO = PreparePaymentResDTO.from(result);
+
+        return ResponseEntity.ok(resDTO);
+    }
+
+    @Override
+    @PostMapping("/confirm")
+    public ResponseEntity<ConfirmPaymentResDTO> confirmPayment(
+            @CurrentMember Long currentMemberId,
+            @RequestBody ConfirmPaymentReqDTO reqDTO
+    ) {
+        ConfirmPaymentUseCase.Param param = reqDTO.toParam(currentMemberId);
+        ConfirmPaymentUseCase.Result result = confirmPaymentUseCase.execute(param);
+        ConfirmPaymentResDTO resDTO = ConfirmPaymentResDTO.from(result);
 
         return ResponseEntity.ok(resDTO);
     }
--- payment-service/src/main/java/com/lgcns/bebee/payment/presentation/dto/req/PreparePaymentReqDTO.java
+++ payment-service/src/main/java/com/lgcns/bebee/payment/presentation/dto/req/PreparePaymentReqDTO.java
@@ -6,20 +6,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "결제 준비 요청 DTO")
 public record PreparePaymentReqDTO(
         @Schema(
-                description = "회원 ID",
-                example = "100",
-                requiredMode = Schema.RequiredMode.REQUIRED
-        )
-        Long memberId,
-
-        @Schema(
                 description = "충전 금액",
                 example = "10000",
                 requiredMode = Schema.RequiredMode.REQUIRED
         )
         Integer amount
 ) {
-    public PreparePaymentUseCase.Param toParam() {
+    public PreparePaymentUseCase.Param toParam(Long memberId) {
         return new PreparePaymentUseCase.Param(memberId, amount);
     }
 }
