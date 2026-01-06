e90c8d2f8d9fc74a463e11501b2215b5f5f793bd
MinJaeSon <mjsonsj@gmail.com>
2026-01-06 11:10:20 +0900

    chore: 응답 dto에서 json 직렬화 시 Long -> String 타입으로 변환

--- match-service/src/main/java/com/lgcns/bebee/match/presentation/dto/res/HelperApplicationPostsGetResDTO.java
+++ match-service/src/main/java/com/lgcns/bebee/match/presentation/dto/res/HelperApplicationPostsGetResDTO.java
@@ -10,9 +10,40 @@ import java.util.List;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class HelperApplicationPostsGetResDTO {
-    private List<GetHelperApplicationPostsUseCase.PostSummary> posts;
+    private List<PostSummaryDTO> posts;
 
     public static HelperApplicationPostsGetResDTO from(GetHelperApplicationPostsUseCase.Result result) {
-        return new HelperApplicationPostsGetResDTO(result.getPosts());
+         List<PostSummaryDTO> posts = result.getPosts().stream()
+                .map(PostSummaryDTO::from).toList();
+
+        return new HelperApplicationPostsGetResDTO(posts);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class PostSummaryDTO {
+        private String postId;
+        private String title;
+        private String region;
+        private Integer commonApplicantCount;
+        private Integer volunteerApplicantCount;
+        private Boolean isMatched;
+        private Integer daysRemaining;
+        private Object engagementTime;
+        private List<Integer> helpCategories;
+
+        public static PostSummaryDTO from(GetHelperApplicationPostsUseCase.PostSummary summary) {
+            return new PostSummaryDTO(
+                    String.valueOf(summary.getPostId()),
+                    summary.getTitle(),
+                    summary.getRegion(),
+                    summary.getCommonApplicantCount(),
+                    summary.getVolunteerApplicantCount(),
+                    summary.getIsMatched(),
+                    summary.getDaysRemaining(),
+                    summary.getEngagementTime(),
+                    summary.getHelpCategories()
+            );
+        }
     }
 }
