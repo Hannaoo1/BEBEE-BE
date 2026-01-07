b2ef49ffaf05ec3086100e40117e857fbf0b032a
khw7385 <khw7385@naver.com>
2026-01-07 10:18:53 +0900

    fix: payment 설정 오류 해결

--- payment-service/src/main/java/com/lgcns/bebee/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/lgcns/bebee/payment/PaymentServiceApplication.java
@@ -2,11 +2,13 @@ package com.lgcns.bebee.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.lgcns.bebee")
+@ConfigurationPropertiesScan(basePackages = "com.lgcns.bebee")
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
---
+++ payment-service/src/main/java/com/lgcns/bebee/payment/common/config/TSIDConfig.java
@@ -0,0 +1,8 @@
+package com.lgcns.bebee.payment.common.config;
+
+import com.lgcns.bebee.common.data.config.BaseTSIDConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TSIDConfig extends BaseTSIDConfig {
+}
