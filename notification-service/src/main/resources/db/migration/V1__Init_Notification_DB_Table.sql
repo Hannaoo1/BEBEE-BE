9875c3cd657d089831ae87a33903919ff1c71150
khw7385 <khw7385@naver.com>
2026-01-08 15:21:03 +0900

    docs: notification-service flyway 문서 수정

--- notification-service/src/main/resources/db/migration/V1__Init_Notification_DB_Table.sql
+++ notification-service/src/main/resources/db/migration/V1__Init_Notification_DB_Table.sql
@@ -1,67 +1,67 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 # DROP TABLES
-DROP TABLE IF EXISTS application_in_app_notification;
-DROP TABLE IF EXISTS chat_in_app_notification;
-DROP TABLE IF EXISTS match_in_app_notification;
-DROP TABLE IF EXISTS in_app_notification;
+DROP TABLE IF EXISTS application_app_notification;
+DROP TABLE IF EXISTS chat_app_notification;
+DROP TABLE IF EXISTS match_app_notification;
+DROP TABLE IF EXISTS app_notification;
 DROP TABLE IF EXISTS push_notification_subscription;
 
-# 1. InAppNotification 테이블
-CREATE TABLE in_app_notification
+# 1. AppNotification 테이블
+CREATE TABLE app_notification
 (
-    in_app_notification_id BIGINT       NOT NULL PRIMARY KEY,
-    receiver_id            BIGINT       NOT NULL,
-    sender_id              BIGINT       NOT NULL,
-    is_read                BOOLEAN      NOT NULL,
-    read_at                TIMESTAMP,
-    type                   ENUM ('CHAT', 'APPLICATION', 'MATCH'),
-    created_at             TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at             TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    app_notification_id BIGINT       NOT NULL PRIMARY KEY,
+    receiver_id         BIGINT       NOT NULL,
+    sender_id           BIGINT       NOT NULL,
+    is_read             BOOLEAN      NOT NULL,
+    read_at             TIMESTAMP,
+    type                ENUM ('CHAT', 'APPLICATION', 'MATCH'),
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-# 2. ChatInAppNotification 테이블
-CREATE TABLE chat_in_app_notification
+# 2. ChatAppNotification 테이블
+CREATE TABLE chat_app_notification
 (
-    in_app_notification_id BIGINT NOT NULL PRIMARY KEY,
-    chat_room_id BIGINT NOT NULL,
-    message_preview VARCHAR(255),
-    message_count INT NOT NULL DEFAULT 1,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    app_notification_id BIGINT NOT NULL PRIMARY KEY,
+    chat_room_id        BIGINT NOT NULL,
+    message_preview     VARCHAR(255),
+    message_count       INT NOT NULL DEFAULT 1,
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT FK_chat_TO_notification
-       FOREIGN KEY (in_app_notification_id)
-           REFERENCES in_app_notification(in_app_notification_id)
+       FOREIGN KEY (app_notification_id)
+           REFERENCES app_notification(app_notification_id)
            ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-# 3. ApplicationInAppNotification 테이블
-CREATE TABLE application_in_app_notification
+# 3. ApplicationAppNotification 테이블
+CREATE TABLE application_app_notification
 (
-  in_app_notification_id BIGINT NOT NULL PRIMARY KEY,
-  application_id BIGINT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  app_notification_id BIGINT NOT NULL PRIMARY KEY,
+  application_id      BIGINT NOT NULL,
+  created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
   CONSTRAINT FK_application_TO_notification
-      FOREIGN KEY (in_app_notification_id)
-          REFERENCES in_app_notification(in_app_notification_id)
+      FOREIGN KEY (app_notification_id)
+          REFERENCES app_notification(app_notification_id)
           ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-# 4. MatchInAppNotification 테이블
-CREATE TABLE match_in_app_notification
+# 4. MatchAppNotification 테이블
+CREATE TABLE match_app_notification
 (
-    in_app_notification_id BIGINT NOT NULL PRIMARY KEY,
-    match_id BIGINT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    app_notification_id BIGINT NOT NULL PRIMARY KEY,
+    match_id            BIGINT NOT NULL,
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
     CONSTRAINT FK_match_TO_notification
-        FOREIGN KEY (in_app_notification_id)
-            REFERENCES in_app_notification(in_app_notification_id)
+        FOREIGN KEY (app_notification_id)
+            REFERENCES app_notification(app_notification_id)
             ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
