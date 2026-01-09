f8e1e804a0b70e1bed102c3fce2b74b9e3f564fc
MinJaeSon <mjsonsj@gmail.com>
2026-01-09 12:52:58 +0900

    add: 매칭 확인서 수락 이벤트 핸들러

---
+++ payment-service/src/main/java/com/lgcns/bebee/payment/infrastructure/event/AgreementConfirmedEventHandler.java
@@ -0,0 +1,30 @@
+package com.lgcns.bebee.payment.infrastructure.event;
+
+
+import com.lgcns.bebee.common.data.event.AgreementConfirmedEvent;
+import com.lgcns.bebee.common.data.event.EventHandler;
+import com.lgcns.bebee.common.data.event.EventType;
+import com.lgcns.bebee.payment.application.usecase.UseHoneyUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AgreementConfirmedEventHandler implements EventHandler<AgreementConfirmedEvent> {
+
+    private final UseHoneyUseCase useHoneyUseCase;
+
+    @Override
+    public EventType getEventType() {
+        return EventType.AGREEMENT_CONFIRMED;
+    }
+
+    @Override
+    public void handle(AgreementConfirmedEvent event) {
+        new UseHoneyUseCase.Param(
+                event.getDisabledId(),
+                event.getMatchId(),
+                event.getUseHoney()
+        );
+    }
+}
