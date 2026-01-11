ebcaff7fd9af1e1a0a2493a64389222671468b4d
khw7385 <khw7385@naver.com>
2026-01-11 19:56:31 +0900

    feat: 매칭 확인서 거절 이벤트에 대한 채팅 서비스 로직 구현

--- chat-service/src/main/java/com/lgcns/bebee/chat/infrastructure/event/AgreementRefusedEventHandler.java
+++ chat-service/src/main/java/com/lgcns/bebee/chat/infrastructure/event/AgreementRefusedEventHandler.java
@@ -1,9 +1,7 @@
 package com.lgcns.bebee.chat.infrastructure.event;
 
-import com.lgcns.bebee.chat.application.UpdateChatroomMatchStatusUseCase;
-import com.lgcns.bebee.chat.domain.entity.sync.MatchStatusSync;
-import com.lgcns.bebee.common.data.event.AgreementCreatedEvent;
-import com.lgcns.bebee.common.data.event.AgreementRefusedEvent;
+import com.lgcns.bebee.chat.application.ProcessAgreementRefusedUseCase;
+import com.lgcns.bebee.common.data.event.match.AgreementRefusedEvent;
 import com.lgcns.bebee.common.data.event.EventHandler;
 import com.lgcns.bebee.common.data.event.EventType;
 import lombok.RequiredArgsConstructor;
@@ -12,15 +10,21 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class AgreementRefusedEventHandler implements EventHandler<AgreementRefusedEvent> {
-    private final UpdateChatroomMatchStatusUseCase updateChatroomMatchStatusUseCase;
+    private final ProcessAgreementRefusedUseCase processAgreementRefusedUseCase;
 
     @Override
     public EventType getEventType() { return EventType.AGREEMENT_REFUSED;}
 
     @Override
     public void handle(AgreementRefusedEvent event) {
-        UpdateChatroomMatchStatusUseCase.Param param = new UpdateChatroomMatchStatusUseCase.Param(event.getChatroomId(), MatchStatusSync.NON_MATCHED);
+        ProcessAgreementRefusedUseCase.Param param = new ProcessAgreementRefusedUseCase.Param(
+                event.getChatroomId(),
+                event.getChatId(),
+                event.getDisabledId(),
+                event.getHelperId(),
+                event.getCreatedAt()
+        );
 
-        updateChatroomMatchStatusUseCase.execute(param);
+        processAgreementRefusedUseCase.execute(param);
     }
 }
