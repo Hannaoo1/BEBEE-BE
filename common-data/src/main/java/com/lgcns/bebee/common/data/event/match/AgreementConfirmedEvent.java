46a36865f59fb2d229fbad2ea90f5c012a157ac8
khw7385 <khw7385@naver.com>
2026-01-11 20:05:23 +0900

    merge: 병합 충돌 해결

--- common-data/src/main/java/com/lgcns/bebee/common/data/event/match/AgreementConfirmedEvent.java
+++ common-data/src/main/java/com/lgcns/bebee/common/data/event/match/AgreementConfirmedEvent.java
@@ -10,7 +10,12 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class AgreementConfirmedEvent extends DomainEvent {
     private final Long chatroomId;
+<<<<<<< HEAD:common-data/src/main/java/com/lgcns/bebee/common/data/event/match/AgreementConfirmedEvent.java
     private final Long chatId;
+=======
+
+    private final Long helperId;
+>>>>>>> 8e0a30f (feat: AgreementConfirmed 이벤트 처리 및 Payment sync 로직 구현):common-data/src/main/java/com/lgcns/bebee/common/data/event/AgreementConfirmedEvent.java
     private final Long disabledId;
     private final Long helperId;
     private final LocalDateTime createdAt;
