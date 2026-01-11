583d04caafca62a1ab88701a29efdf131e5ceea7
khw7385 <khw7385@naver.com>
2026-01-11 19:48:51 +0900

    refactor: sqs 이벤트 리스너 리팩토링 이벤트 타입을 메시지 본문이 아닌 헤더에서 접근하게 변경

--- match-service/src/main/java/com/lgcns/bebee/match/infrastructure/event/aws/SqsEventListener.java
+++ match-service/src/main/java/com/lgcns/bebee/match/infrastructure/event/aws/SqsEventListener.java
@@ -11,6 +11,8 @@ import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -22,31 +24,16 @@ public class SqsEventListener {
     private final EventHandlerRegistry handlerRegistry;
 
     @SqsListener("${app.sqs.match-queue-url}")
-    public void handleEvent(String message) {
+    public void handleEvent(@Payload String payload, @Header("eventType") String eventType) {
         try{
-            log.info("SQS 메시지 수신: {}", message);
-
-            JsonNode root = objectMapper.readTree(message);
-
-            String snsType = root.path("Type").asText();
-            if (!"Notification".equals(snsType)) {
-                log.info("SNS Notification 아님. 무시합니다. type={}", snsType);
-                return;
-            }
-
-            JsonNode attributes = root.get("MessageAttributes");
-            String eventType = attributes.get("eventType").get("Value").asText();
-
-            String payload = root.get("Message").asText();
-
-            log.info("이벤트 타입: {}, 페이로드: {}", eventType, payload);
+            log.info("SQS 이벤트 수신 - 타입: {}, 페이로드: {}", eventType, payload);
 
             EventType type = EventType.from(eventType);
             DomainEvent event = objectMapper.readValue(payload, type.getEventClass());
 
             processEvent(event, type);
         } catch (JsonProcessingException e) {
-            log.error("SQS 메시지 처리 실패: {}", message, e);
+            log.error("SQS 메시지 처리 실패: {}", payload, e);
             throw new RuntimeException(e);
         }
     }
