c794bb6cde173219aeb769aece0b4ae9a1429f9d
hanna <hannaj5494@gmail.com>
2026-01-11 16:12:24 +0900

    feat: 뱃지 컨트롤러 생성

---
+++ match-service/src/main/java/com/lgcns/bebee/match/presentation/BadgeController.java
@@ -0,0 +1,33 @@
+package com.lgcns.bebee.match.presentation;
+
+import com.lgcns.bebee.common.annotation.CurrentMember;
+import com.lgcns.bebee.match.application.usecase.GetHelperBadgesUseCase;
+import com.lgcns.bebee.match.presentation.dto.res.HelperBadgesGetResDTO;
+import com.lgcns.bebee.match.presentation.swagger.BadgeSwagger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/badges")
+@RequiredArgsConstructor
+public class BadgeController implements BadgeSwagger {
+
+    private final GetHelperBadgesUseCase getHelperBadgesUseCase;
+
+    @GetMapping
+    @Override
+    public ResponseEntity<HelperBadgesGetResDTO> getHelperBadges(
+            @CurrentMember Long currentMemberId
+    ) {
+
+        GetHelperBadgesUseCase.Param param = new GetHelperBadgesUseCase.Param(currentMemberId);
+        GetHelperBadgesUseCase.Result result = getHelperBadgesUseCase.execute(param);
+
+        HelperBadgesGetResDTO response = HelperBadgesGetResDTO.from(result.getBadges());
+
+        return ResponseEntity.ok(response);
+    }
+}
