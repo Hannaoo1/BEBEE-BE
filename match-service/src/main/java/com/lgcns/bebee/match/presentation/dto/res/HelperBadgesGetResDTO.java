6bcf6bcfb0bb54aae8cdf1a843c5c397b209095d
hanna <hannaj5494@gmail.com>
2026-01-11 16:46:30 +0900

    refactor: 뱃지 dto 수정

--- match-service/src/main/java/com/lgcns/bebee/match/presentation/dto/res/HelperBadgesGetResDTO.java
+++ match-service/src/main/java/com/lgcns/bebee/match/presentation/dto/res/HelperBadgesGetResDTO.java
@@ -1,9 +1,12 @@
 package com.lgcns.bebee.match.presentation.dto.res;
 
+import com.lgcns.bebee.match.application.usecase.GetHelperBadgesUseCase;
+import com.lgcns.bebee.match.domain.entity.Badge;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -11,7 +14,19 @@ import java.util.List;
 public class HelperBadgesGetResDTO {
     private List<BadgeStatusDTO> badges;
 
-    public static HelperBadgesGetResDTO from(List<BadgeStatusDTO> badges) {
-        return new HelperBadgesGetResDTO(badges);
+    public static HelperBadgesGetResDTO from(GetHelperBadgesUseCase.Result result) {
+        List<BadgeStatusDTO> badgeStatuses = new ArrayList<>();
+
+        // 존재하는 뱃지를 DTO로 변환
+        for (Badge badge : result.getExistingBadges()) {
+            badgeStatuses.add(BadgeStatusDTO.from(badge));
+        }
+
+        // 없는 카테고리에 대해 빈 뱃지 생성
+        for (Long categoryId : result.getMissingCategoryIds()) {
+            badgeStatuses.add(BadgeStatusDTO.ofEmpty(categoryId));
+        }
+
+        return new HelperBadgesGetResDTO(badgeStatuses);
     }
 }
