aeb8db55f279be479f859a04014d716fb579c7c9
hanna <hannaj5494@gmail.com>
2026-01-12 12:28:24 +0900

    chore: 로깅 추가

--- member-service/src/main/java/com/lgcns/bebee/member/infrastructure/event/EngagementCompletedEventHandler.java
+++ member-service/src/main/java/com/lgcns/bebee/member/infrastructure/event/EngagementCompletedEventHandler.java
@@ -5,8 +5,10 @@ import com.lgcns.bebee.common.data.event.EventHandler;
 import com.lgcns.bebee.common.data.event.EventType;
 import com.lgcns.bebee.member.application.usecase.CreateBadgeUseCase;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class EngagementCompletedEventHandler implements EventHandler<EngagementCompletedEvent> {
@@ -19,7 +21,9 @@ public class EngagementCompletedEventHandler implements EventHandler<EngagementC
 
     @Override
     public void handle(EngagementCompletedEvent event) {
+        log.info("활동 완료 이벤트 처리 시작 - agreementId: {}", event.getAgreementId());
         CreateBadgeUseCase.Param param = new CreateBadgeUseCase.Param(event.getAgreementId());
         createBadgeUseCase.execute(param);
+        log.info("활동 완료 이벤트 처리 완료 - agreementId: {}", event.getAgreementId());
     }
 }
