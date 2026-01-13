c169127ece05f43233378ecd3c7a97edba57430d
khw7385 <khw7385@naver.com>
2026-01-13 13:27:06 +0900

    fix: 알림 서비스 - Notification Controller 생성자 추가

--- notification-service/src/main/java/com/lgcns/bebee/notification/presentation/NotificationController.java
+++ notification-service/src/main/java/com/lgcns/bebee/notification/presentation/NotificationController.java
@@ -4,14 +4,17 @@ import com.lgcns.bebee.common.annotation.CurrentMember;
 import com.lgcns.bebee.notification.application.RegisterFcmTokenUseCase;
 import com.lgcns.bebee.notification.presentation.dto.req.FcmTokenRegisterReqDTO;
 import com.lgcns.bebee.notification.presentation.swagger.NotificationSwagger;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/notifications")
+@RequiredArgsConstructor
 public class NotificationController implements NotificationSwagger {
-    private RegisterFcmTokenUseCase registerFcmTokenUseCase;
+    private final RegisterFcmTokenUseCase registerFcmTokenUseCase;
 
     @PostMapping("/fcm/tokens")
     public ResponseEntity<Void> registerToken(
