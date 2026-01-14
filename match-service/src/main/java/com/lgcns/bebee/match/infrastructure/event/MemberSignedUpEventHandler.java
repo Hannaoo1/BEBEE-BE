2b132c18c423d6f52daadf345f8630f9826b3b03
khw7385 <khw7385@naver.com>
2026-01-14 12:47:54 +0900

    fix: 매칭 서비스 - 회원 회원가입 이벤트 핸들러 구현

--- match-service/src/main/java/com/lgcns/bebee/match/infrastructure/event/MemberSignedUpEventHandler.java
+++ match-service/src/main/java/com/lgcns/bebee/match/infrastructure/event/MemberSignedUpEventHandler.java
@@ -2,12 +2,14 @@ package com.lgcns.bebee.match.infrastructure.event;
 
 import com.lgcns.bebee.common.data.event.EventHandler;
 import com.lgcns.bebee.common.data.event.member.MemberSignedUpEvent;
+import com.lgcns.bebee.match.application.usecase.CreateMemberUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MemberSignedUpEventHandler implements EventHandler<MemberSignedUpEvent> {
+    private final CreateMemberUseCase createMemberUseCase;
 
     @Override
     public Class<MemberSignedUpEvent> getEventClass() {
@@ -16,6 +18,21 @@ public class MemberSignedUpEventHandler implements EventHandler<MemberSignedUpEv
 
     @Override
     public void handle(MemberSignedUpEvent event) {
+        CreateMemberUseCase.Param param = new CreateMemberUseCase.Param(
+                event.getMemberId(),
+                event.getNickname(),
+                event.getGender(),
+                event.getRole(),
+                event.getBirthDate(),
+                event.getLatitude(),
+                event.getLongitude(),
+                event.getProfileImageUrl(),
+                event.getAddressRoad(),
+                event.getLegalDongCode(),
+                event.getDisabilityCategoryIds(),
+                event.getHelpCategoryIds()
+        );
 
+        createMemberUseCase.execute(param);
     }
 }
